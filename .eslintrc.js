86a5f77c3dcaab71fece101a33677de917487bf9
Batiste Bieler <batiste.bieler@gmail.com>
2019-11-10 17:49:50 +0100

    Move stuff in tests

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "globals": {
+    "window": true,
+  },
+  "rules": {
+    "no-plusplus": "off",
+    "no-unused-expressions": "off",
+    "quote-props": "off",
+    "camelcase": "off",
+    "no-underscore-dangle": "off",
+    "no-param-reassign": "off",
+    "no-shadow": "off",
+    // for the generated parser
+    "consistent-return": "off",
+    "object-property-newline": "off",
+    "dot-notation": "off",
+    "no-use-before-define": "off",
+    "no-console": ["error", { allow: ["log", "error", "warn"] }]
+  },
+  "plugins": [
+    "ie11"
+  ]
+};
--- tests/test.js
+++ tests/test.js
@@ -1,28 +1,27 @@
 const parser = require('./parser')
 const { tokensDefinition } = require('./tokensDefinition')
-const { displayError } = require('./utils')
-const { grammar } = require('./grammar')
 
 function parse(code) {
   let tokens = parser.tokenize(tokensDefinition, code)
   const ast = parser.parse(tokens)
-  if(!ast.success) {
-    displayError(tokens, tokensDefinition, grammar, ast)
-  }
   return ast
 }
 
-let code = `9+9
+test('correct grammar', () => {
+  let code = `9+9
 1+1+2
-`
+`  
+  const result = parse(code)
+  expect(result.success).toBe(true)
+})
 
-parse(code)
-
-code = `9+9
+test('incorrect', () => {
+  code = `9+9
 1+1
 1+2
 1 + 4
 `
-
-parse(code)
+  const result = parse(code)
+  expect(result.success).toBe(false)
+})
 
